536fc59421b2122a4f14d6d3ff7caac63ab9046b
Tyler Kokoszka <techkoko77@gmail.com>
2019-06-30 22:26:31 -0400

    update gradle build

--- src/test/java/com/impurityonline/gateway/GatewayApplicationTests.java
+++ src/test/java/com/impurityonline/gateway/GatewayApplicationTests.java
@@ -2,15 +2,27 @@ package com.impurityonline.gateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GatewayApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
 	@Test
 	public void contextLoads() {
+		assertNotNull(context);
 	}
 
+	@Test
+	public void entryIsValid() {
+		assertDoesNotThrow(() -> GatewayApplication.main(new String[0]));
+	}
 }
