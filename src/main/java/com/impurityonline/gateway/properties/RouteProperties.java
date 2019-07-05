ad005c51d7968aafffef7b71ffd4d1030948d18d
Tyler Kokoszka <techkoko77@gmail.com>
2019-07-05 15:12:04 -0400

    Update RouteProperties.java

--- src/main/java/com/impurityonline/gateway/properties/RouteProperties.java
+++ src/main/java/com/impurityonline/gateway/properties/RouteProperties.java
@@ -2,10 +2,12 @@ package com.impurityonline.gateway.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@Component
 @ConfigurationProperties(prefix = "route")
 public class RouteProperties {
     @NotBlank
