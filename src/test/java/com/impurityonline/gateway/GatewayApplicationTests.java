d7ca44c75eb6b22d1fef8e5d4edd8a719b2cde93
Tyler Kokoszka <techkoko77@gmail.com>
2019-07-11 20:36:51 -0400

    Fixed junit 5 setup

--- src/test/java/com/impurityonline/gateway/GatewayApplicationTests.java
+++ src/test/java/com/impurityonline/gateway/GatewayApplicationTests.java
@@ -1,15 +1,12 @@
 package com.impurityonline.gateway;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class GatewayApplicationTests {
 
